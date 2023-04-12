1ff138ef207f7a8422801f241c10649e4b08f44f
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-04-12 22:56:18 +0200

    searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,13 +3,27 @@ import { Component } from 'react';
 import css from './Searchbar.module.css';
 
 export class Searchbar extends Component {
-  handleSubmit = () => {};
+  handleSubmit = event => {
+    event.preventDefault();
+    const response = axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '33257268-27ad9fcecc17d6e2546f4b9dc',
+        q: search,
+        image_type: 'photo',
+        orientation: 'horizonatal',
+        safesearch: true,
+        per_page,
+        page,
+      },
+    });
+  };
 
   render() {
+    const { imageSearch } = this.props;
     return (
       <header className={css.searchbar}>
         <form className={css.form}>
-          <button type="submit" className={css.button}>
+          <button type="submit" className={css.button} onSubmit={imageSearch}>
             <span className={css.buttonLabel}>Search</span>
           </button>
 
